8a5833a13f1af4dec6a436e97fecad8a80a64c79
Albert Croft <acroft@cyber-wizard.com>
2017-04-21 22:15:59 -0500

    tk_articulated.pl: Add control to select hour mode (12/24hr) and articulation mode (smh/hms inner->outer) (working version 20170419).

--- tk_articulated_clock.pl
+++ tk_articulated_clock.pl
@@ -8,7 +8,7 @@ use Getopt::Long;
 use Tk;
 use Tk::Notebook;
 
-use vars qw( %obj $_debug $_test $_verbose );
+use vars qw( %clock %obj $_debug $_test $_verbose $direction_ltg $mode_24h );
 
 $| = 1;
 srand();
@@ -20,6 +20,10 @@ $_debug   = 0;
 $_test    = 0;
 $_verbose = 0;
 
+$mode_24h           = 0;
+$direction_ltg      = 0;
+$obj{setting}{time} = q{00:00:00};
+
 my %max = (
     x => 480,
     y => 500,
@@ -27,16 +31,38 @@ my %max = (
 
 GetOptions( qq{debug} => \$_debug, );
 
-
 $obj{mw} = Tk::MainWindow->new();
+
+$obj{display} =
+  $obj{mw}->Frame()->grid( -column => 0, -columnspan => 4, -row => 0, );
 $obj{canvas} =
-  $obj{mw}
+  $obj{display}
   ->Canvas( -width => $max{x}, -height => $max{y}, -background => q{#000000}, )
   ->pack();
 $obj{image} = $obj{mw}
   ->Photo( -width => $max{x}, -height => $max{y}, -palette => q{256/256/256}, );
 $obj{image}->blank;
 $obj{canvas}->createImage( 0, 0, -image => $obj{image}, -anchor => q{nw}, );
+
+$obj{control} =
+  $obj{mw}->Frame()->grid( -column => 0, -columnspan => 4, -row => 6, );
+
+$obj{control}->Label( -text => q{Hour mode:} )
+  ->grid( -column => 0, -columnspan => 2, -row => 1, );
+$obj{control}
+  ->Radiobutton( -text => q{24h}, -value => 1, -variable => \$mode_24h, )
+  ->grid( -column => 2, -row => 1, );
+$obj{control}
+  ->Radiobutton( -text => q{12h}, -value => 0, -variable => \$mode_24h, )
+  ->grid( -column => 3, -row => 1, );
+$obj{control}->Label( -text => q{Articulation mode:} )
+  ->grid( -column => 0, -columnspan => 2, -row => 2, );
+$obj{control}
+  ->Radiobutton( -text => q{smh}, -value => 1, -variable => \$direction_ltg, )
+  ->grid( -column => 2, -row => 2, );
+$obj{control}
+  ->Radiobutton( -text => q{hms}, -value => 0, -variable => \$direction_ltg, )
+  ->grid( -column => 3, -row => 2, );
 
 $obj{canvas}->after( 1250, \&update );
 
@@ -49,9 +75,10 @@ MainLoop;
 #
 sub update {
 
-    my $_pi      = 3.141593;
-    my $mode_24h = 0;
-    my %color    = (
+    my $_pi = 3.141593;
+
+    # my $mode_24h = 0;
+    my %color = (
 
         # hand => q{#00FFFF},
         face => q{#009999},
@@ -122,8 +149,9 @@ sub update {
     foreach my $i ( 1 .. ( $mode_24h ? 24 : 12 ) ) {
         my %cp;
         $cp{radius} = $radius + 40;
+
         # $cp{angle}  = $_pi * ( ( 24 - $i - 6 ) / 12 );
-        if ( $mode_24h ) {
+        if ($mode_24h) {
             $cp{angle} = $_pi * ( ( 6 - $i ) / 12 );
         }
         else {
@@ -143,41 +171,67 @@ sub update {
     {
         my %inner;
         my %outer;
+        
+        my @order = qw/ hour minute second /;
+        if ( $direction_ltg ) {
+            @order = reverse @order;
+        }
 
-        $inner{hour}{radius}   = int( $radius * ( 3.0 / 6.0 ) );
-        $inner{minute}{radius} = $outer{hour}{radius} =   int( $radius * ( 2.0 / 6.0 ) );
-        $inner{second}{radius} = $outer{minute}{radius} = int( $radius * ( 1.0 / 6.0 ) );
+        $inner{hour}{radius} = int( $radius * ( 3.0 / 6.0 ) );
+        $inner{minute}{radius} = $outer{hour}{radius} =
+          int( $radius * ( 2.0 / 6.0 ) );
+        $inner{second}{radius} = $outer{minute}{radius} =
+          int( $radius * ( 1.0 / 6.0 ) );
         $outer{second}{radius} = $radius;
         $color{hand}{hour}     = q{#00FF00};
         $color{hand}{minute}   = q{#0000FF};
         $color{hand}{second}   = q{#FF0000};
-        $obj{canvas}->createOval( $center{x} - 5, $center{y} - 5, $center{x} + 5, $center{y} + 5, -fill => $color{hand}{hour}, );
-        foreach my $str (qw/ hour minute second /) {
+        $obj{canvas}->createOval(
+            $center{x} - 5,
+            $center{y} - 5,
+            $center{x} + 5,
+            $center{y} + 5,
+            -fill => $color{hand}{hour},
+        );
+        
+        $inner{x} = $outer{x} = $center{x};
+        $inner{y} = $outer{y} = $center{y};
+
+        foreach my $str (@order) {
             my $angle;
-                # =MOD( INT( 90 - 360 * ( D2 / 24 ) ), 360 )
-                if ( $mode_24h ) {
-                    $angle = $_pi * ( int( 90 - 360 * ( ( $lt[2] % 24 )/ 24.0 ) ) % 360 ) / 180.0;
-                }
-                else {
-                    $angle = $_pi * ( int( 90 - 360 * ( ( $lt[2] % 12 )/ 12.0 ) ) % 360 ) / 180.0;
-                }
-            $inner{x} = $center{x};
-            $inner{y} = $center{y};
+
+            # =MOD( INT( 90 - 360 * ( D2 / 24 ) ), 360 )
+            if ($mode_24h) {
+                $angle =
+                  $_pi * ( int( 90 - 360 * ( ( $lt[2] % 24 ) / 24.0 ) ) % 360 )
+                  / 180.0;
+            }
+            else {
+                $angle =
+                  $_pi * ( int( 90 - 360 * ( ( $lt[2] % 12 ) / 12.0 ) ) % 360 )
+                  / 180.0;
+            }
+            $inner{x} = $outer{x};
+            $inner{y} = $outer{y};
             if ( $str eq q{minute} ) {
+
                 # =MOD( INT( 90 - 360 * ( D2 / 60 ) ), 360 )
-                $angle = $_pi * ( int( 90 - 360 * ( $lt[1] / 60.0 ) ) % 360 ) / 180.0;
+                $angle =
+                  $_pi * ( int( 90 - 360 * ( $lt[1] / 60.0 ) ) % 360 ) / 180.0;
                 $inner{x} = $outer{x};
                 $inner{y} = $outer{y};
             }
             if ( $str eq q{second} ) {
+
                 # =MOD( INT( 90 - 360 * ( D2 / 60 ) ), 360 )
-                $angle = $_pi * ( int( 90 - 360 * ( $lt[0] / 60.0 ) ) % 360 ) / 180.0;
+                $angle =
+                  $_pi * ( int( 90 - 360 * ( $lt[0] / 60.0 ) ) % 360 ) / 180.0;
                 $inner{x} = $outer{x};
                 $inner{y} = $outer{y};
             }
             $outer{x} = $inner{x} + int( $inner{$str}{radius} * cos($angle) );
             $outer{y} = $inner{y} - int( $inner{$str}{radius} * sin($angle) );
-            if ( $str ne q{hour} ) {
+            # if ( $str ne q{hour} ) {
                 $obj{canvas}->createOval(
                     $inner{x} - 3,
                     $inner{y} - 3,
@@ -185,7 +239,7 @@ sub update {
                     $inner{y} + 3,
                     -fill => $color{hand}{$str},
                 );
-            }
+            # }
             $obj{canvas}->createLine(
                 $inner{x}, $inner{y}, $outer{x}, $outer{y},
                 -fill  => $color{hand}{$str},
@@ -207,3 +261,4 @@ BEGIN {
     }
 }
 
+__END__
